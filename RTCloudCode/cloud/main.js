fa16dac52ce73eed45ca322f8e3d8b09590947ec
yiqin <yiqin.mems@gmail.com>
2014-06-24 21:30:32 -0400

    Release 2.0.2

--- RTCloudCode/cloud/main.js
+++ RTCloudCode/cloud/main.js
@@ -25,21 +25,5 @@ Parse.Cloud.define("sendMessageToTwillio", function(request, response) {
 	  }
 	);
 
-	var client2 = require('twilio')('AC840912e124927b5100895bc5b9a67f64', 'f83f5ef557fe90f1b1b4c6cc4d569d5c');
-	// Send an SMS message
-	client2.sendSms({
-		to:'+17654041448', 
-	    from: '+13177080548', 
-	    body: request.params.order
-	  }, function(err, responseData) { 
-	    if (err) {
-	      console.log(err);
-	    } else { 
-	      console.log(responseData.from); 
-	      console.log(responseData.body);
-	    }
-	  }
-	);
-
 	response.success(request.params.order);
 });
